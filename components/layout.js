af6a912cba0e1bf35893a9c3f32eff097222ca03
Iain Collins <me@iaincollins.com>
2023-07-29 04:22:43 +0100

    Improve CSS and metadata

--- components/layout.js
+++ components/layout.js
@@ -10,9 +10,9 @@ export default ({
   <>
     <Head>
       <title>{title}</title>
-      <meta name='description' content='Ardent Industry provides trade and exploration data for Elite Dangerous. Find the best prices for anything in the galaxy!' />
-      <meta name='keywords' content='Ardent Industry, Elite Dangerous, Elite: Dangerous, market, trade, commodities, pricing, prices, systems, exploration' />
-      <meta name='author' content='Ardent Industry' />
+      <meta name='description' content='Ardent Industry provides trade and exploration data for the game Elite Dangerous' />
+      <meta name='keywords' content='Ardent Industry, Elite Dangerous, Elite: Dangerous, market, trade, commodities, pricing, prices, systems, exploration, API, SQL' />
+      <meta name='author' content='Iain Collins' />
       <meta name='viewport' content='width=device-width' />
     </Head>
     <div className='layout__frame'>
