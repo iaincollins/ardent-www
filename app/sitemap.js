23db50f694f1ffa020c0b40225440b489698959b
Iain Collins <iain.collins@unity3d.com>
2024-09-05 11:39:38 +0100

    Add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,42 @@
+import { getCommodities } from 'lib/commodities'
+
+export default async function sitemap() {
+  const commodities = await getCommodities()
+  const sitemap = [
+    {
+      url: 'https://ardent-industry.com',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1
+    },
+    {
+      url: 'https://ardent-industry.com/commodities',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.7
+    },
+    {
+      url: 'https://ardent-industry.com/about',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.3
+    },
+    {
+      url: 'https://ardent-industry.com/downloads',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.3
+    }
+  ]
+
+  commodities.map(commodity => {
+    sitemap.push({
+      url: `https://ardent-industry.com/commodity/${commodity.commodityName}`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8
+    })
+  })
+
+  return sitemap
+}
