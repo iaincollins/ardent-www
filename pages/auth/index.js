0c6ef7953c7884ba63fe54c34b34ed2c42c752af
Iain Collins <me@iaincollins.com>
2024-10-19 03:04:20 +0100

    Minor refactoring of auth page

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -3,13 +3,13 @@ import Layout from 'components/layout'
 import { API_BASE_URL } from 'lib/consts'
 
 export default () => {
-  const [accessTokenExpires, setAccessTokenExpires] = useState()
+  const [accessTokenExpiryTime, setAccessTokenExpiryTime] = useState()
   const [csrfToken, setCsrfToken] = useState()
 
   useEffect(() => {
     (async () => {
       const accessTokenRes = await fetch(`${API_BASE_URL}/auth/token`, { credentials: 'include' })
-      setAccessTokenExpires((await accessTokenRes.json())?.expires ?? false)
+      setAccessTokenExpiryTime((await accessTokenRes.json())?.expires ?? null)
       const csrfTokenRes = await fetch(`${API_BASE_URL}/auth/csrftoken`, { credentials: 'include' })
       setCsrfToken((await csrfTokenRes.json()).csrfToken)
     })()
@@ -19,22 +19,29 @@ export default () => {
     <Layout >
       <div className='fx__fade-in'>
         <h1>
-          {accessTokenExpires && 'Signed in'}
-          {accessTokenExpires === false && 'Signed out'}
+          {accessTokenExpiryTime && 'Signed in'}
+          {accessTokenExpiryTime === null && 'Signed out'}
         </h1>
-        <p className='clear'>
-          {accessTokenExpires && <>You are signed in. The current access token expires on {accessTokenExpires} but it will auto-refresh.</>}
-          {accessTokenExpires === false && 'You are signed out.'}
-        </p>
-        {accessTokenExpires && csrfToken &&
-          <form method='POST' action={`${API_BASE_URL}/auth/signout`}>
-            <input type='hidden' name='csrfToken' value={csrfToken} />
-            <button className='button'>Sign out</button>
-          </form>}
-        {accessTokenExpires === false &&
-          <form method='GET' action={`${API_BASE_URL}/auth/signin`}>
-            <button className='button'>Sign in</button>
-          </form>}
+        <div className='clear'>
+          {accessTokenExpiryTime &&
+            <>
+              <p>You are signed in.</p>
+              <p>Your current access token from Frontier expires on {new Date(accessTokenExpiryTime).toLocaleString()} but it will auto-refresh.</p>
+              <p>You will not need to approve access again until after you have been signed in for 25 days.</p>
+              {csrfToken &&
+                <form method='POST' action={`${API_BASE_URL}/auth/signout`}>
+                  <input type='hidden' name='csrfToken' value={csrfToken} />
+                  <button className='button'>Sign out</button>
+                </form>}
+            </>}
+          {accessTokenExpiryTime === null &&
+            <>
+              <p>You are signed out.</p>
+              <form method='GET' action={`${API_BASE_URL}/auth/signin`}>
+                <button className='button'>Sign in</button>
+              </form>
+            </>}
+        </div>
       </div>
     </Layout>
   )
