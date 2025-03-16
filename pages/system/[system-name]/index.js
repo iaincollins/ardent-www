793af729da21b8fad14a97656cf10dc9fe044f46
Iain Collins <iain.collins@unity3d.com>
2025-03-16 12:16:11 +0000

    Refactor of system list view to align with new commodity view

--- pages/system/[system-name]/index.js
+++ pages/system/[system-name]/index.js
@@ -60,15 +60,13 @@ export default () => {
   const [tabIndex, setTabIndex] = useState(0)
   const [rareGoods, setRareGoods] = useState([])
 
-  const tabs = ['imports', 'exports', 'nearby']
+  const tabs = ['exports', 'imports', 'nearby']
 
   useEffect(animateTableEffect)
 
   useEffect(() => {
     const basePath = path.basename(router.pathname)
-    if (basePath === 'imports') setTabIndex(0)
-    if (basePath === 'exports') setTabIndex(1)
-    if (basePath === 'nearby') setTabIndex(2)
+    setTabIndex(tabs.indexOf(basePath) === -1 ? 0 : tabs.indexOf(basePath))
   }, [router.pathname])
 
   const onSystemsRowClick = (record, index, event) => {
@@ -423,135 +421,20 @@ export default () => {
           >
             <TabList>
               <Tab>
-                <i style={{lineHeight: '1.5rem', fontSize: '1.5rem', top: '-.15rem', position: 'relative'}} className='icarus-terminal-cargo-import'/>
-                <span className='is-hidden-mobile'>Imports</span>
-                {/* {importOrders && <span className='tab-badge'>{importOrders.length}</span>} */}
-              </Tab>
-              <Tab>
                 <i style={{lineHeight: '1.5rem', fontSize: '1.5rem', top: '-.15rem', position: 'relative'}} className='icarus-terminal-cargo-export'/>
                 <span className='is-hidden-mobile'>Exports</span>
                 {/* {exportOrders && <span className='tab-badge'>{exportOrders.length}</span>} */}
+              </Tab>
+              <Tab>
+                <i style={{lineHeight: '1.5rem', fontSize: '1.5rem', top: '-.15rem', position: 'relative'}} className='icarus-terminal-cargo-import'/>
+                <span className='is-hidden-mobile'>Imports</span>
+                {/* {importOrders && <span className='tab-badge'>{importOrders.length}</span>} */}
               </Tab>
               <Tab>
                 <i style={{lineHeight: '1.5rem', fontSize: '1.5rem', top: '-.15rem', position: 'relative'}} className='icarus-terminal-route'/>
                 <span className='is-hidden-mobile'>Nearby</span>
               </Tab>
             </TabList>
-            {/* {tabIndex !== 2 && <CommodityTabOptions />} */}
-            <TabPanel>
-              {!importOrders && <div style={{ marginTop: '2rem' }} className='loading-bar loading-bar--tab' />}
-              {importOrders &&
-                <Table
-                  className='data-table data-table--striped data-table--interactive data-table--animated'
-                  columns={[
-                    {
-                      title: `Commodities imported (${importOrders.length})`,
-                      dataIndex: 'name',
-                      key: 'name',
-                      align: 'left',
-                      className: 'max-width-mobile',
-                      render: (v, r) =>
-                        <>
-                          <i className='icon icarus-terminal-cargo' />{v}<br />
-                          <div className='is-visible-mobile'>
-                            <small style={{ float: 'right' }}>{r.importOrders.length === 1 ? '1 importer ' : `${r.importOrders.length} importers`}</small>
-                          </div>
-                          <small>
-                            {r.category}
-                            {r?.consumer === true && ' (Consumer)'}
-                          </small>
-                          <div className='is-visible-mobile'>
-                            <table className='data-table--mini data-table--compact two-column-table'>
-                              <tbody style={{ textTransform: 'uppercase' }}>
-                                <tr>
-                                  <td><span className='data-table__label'>Total demand</span>{r.totalDemand > 0 ? `${r.totalDemand.toLocaleString()} T` : <small>{NO_DEMAND_TEXT}</small>}</td>
-                                  <td className='text-right'>
-                                    <span className='data-table__label'>Price</span>
-                                    {r.avgPrice.toLocaleString()} CR
-                                    <br />
-                                    {r.galacticAvgPrice > 0 && r.avgPrice > r.galacticAvgPrice && <small className='commodity__price text-positive'>+ {(r.avgPrice - r.galacticAvgPrice).toLocaleString()} CR</small>}
-                                    {r.avgPrice < r.galacticAvgPrice && <small className='commodity__price text-negative'>- {(r.galacticAvgPrice - r.avgPrice).toLocaleString()} CR</small>}
-                                  </td>
-                                </tr>
-                              </tbody>
-                            </table>
-                            <small style={{ textTransform: 'none' }}>{timeBetweenTimestamps(r.updatedAt)} ago</small>
-                          </div>
-                        </>
-                    },
-                    {
-                      title: 'Importers',
-                      dataIndex: 'importOrders',
-                      key: 'importOrders',
-                      align: 'center',
-                      width: 100,
-                      className: 'is-hidden-mobile',
-                      render: (v) => <span className='muted'>{v.length === 1 ? '1 ' : `${v.length}`}<i style={{ fontSize: '1.25rem', position: 'relative', top: '-.2rem', marginLeft: '.4rem' }} className='icarus-terminal-settlement muted' /></span>
-                    },
-                    {
-                      title: 'Updated',
-                      dataIndex: 'updatedAt',
-                      key: 'updatedAt',
-                      align: 'right',
-                      width: 150,
-                      className: 'is-hidden-mobile no-wrap',
-                      render: (v) => <span style={{ opacity: 0.5 }}>{timeBetweenTimestamps(v)}</span>
-                    },
-                    {
-                      title: 'Total demand',
-                      dataIndex: 'totalDemand',
-                      key: 'totalDemand',
-                      align: 'right',
-                      width: 200,
-                      className: 'is-hidden-mobile no-wrap',
-                      render: (v) => <>{v > 0 ? `${v.toLocaleString()} T` : <small>{NO_DEMAND_TEXT}</small>}</>
-                    },
-                    {
-                      title: 'Avg price',
-                      dataIndex: 'avgPrice',
-                      key: 'avgPrice',
-                      align: 'right',
-                      width: 150,
-                      className: 'is-hidden-mobile no-wrap',
-                      render: (v, r) =>
-                        <>
-                          {v.toLocaleString()} CR
-                          <br />
-                          {r.galacticAvgPrice > 0 && r.avgPrice > r.galacticAvgPrice && <small className='commodity__price text-positive'>+ {(r.avgPrice - r.galacticAvgPrice).toLocaleString()} CR</small>}
-                          {r.avgPrice < r.galacticAvgPrice && <small className='commodity__price text-negative'>- {(r.galacticAvgPrice - r.avgPrice).toLocaleString()} CR</small>}
-                        </>
-                    }
-                  ]}
-                  data={importOrders}
-                  rowKey={(r) => `system_imports_${r.systemAddress}_${r.symbol}`}
-                  expandable={{
-                    expandRowByClick: true,
-                    expandedRowRender: r =>
-                      <>
-                        <p style={{ marginTop: '.5rem' }}>
-                          Demand for <Link href={`/commodity/${r.symbol}/importers?location=${encodeURIComponent(r.systemName)}&maxDistance=100`}>{r.name}</Link> in {r.systemName}
-                        </p>
-                        <LocalCommodityImporters
-                          commodityName={r.name}
-                          commoditySymbol={r.symbol}
-                          commodityOrders={r.importOrders}
-                        />
-                        <Collapsible
-                          trigger={<CollapsibleTrigger>Stock of <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
-                          triggerWhenOpen={<CollapsibleTrigger open>Stock of <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
-                        >
-                          <NearbyCommodityExporters commodity={r} />
-                        </Collapsible>
-                        <Collapsible
-                          trigger={<CollapsibleTrigger>Demand for <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
-                          triggerWhenOpen={<CollapsibleTrigger open>Demand for <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
-                        >
-                          <NearbyCommodityImporters commodity={r} />
-                        </Collapsible>
-                      </>
-                  }}
-                />}
-            </TabPanel>
             <TabPanel>
               {!exportOrders && <div style={{ marginTop: '2rem' }} className='loading-bar loading-bar--tab' />}
               {exportOrders &&
@@ -650,6 +533,120 @@ export default () => {
                           commodityName={r.name}
                           commoditySymbol={r.symbol}
                           commodityOrders={r.exportOrders}
+                        />
+                        <Collapsible
+                          trigger={<CollapsibleTrigger>Stock of <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
+                          triggerWhenOpen={<CollapsibleTrigger open>Stock of <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
+                        >
+                          <NearbyCommodityExporters commodity={r} />
+                        </Collapsible>
+                        <Collapsible
+                          trigger={<CollapsibleTrigger>Demand for <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
+                          triggerWhenOpen={<CollapsibleTrigger open>Demand for <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
+                        >
+                          <NearbyCommodityImporters commodity={r} />
+                        </Collapsible>
+                      </>
+                  }}
+                />}
+            </TabPanel>
+            <TabPanel>
+              {!importOrders && <div style={{ marginTop: '2rem' }} className='loading-bar loading-bar--tab' />}
+              {importOrders &&
+                <Table
+                  className='data-table data-table--striped data-table--interactive data-table--animated'
+                  columns={[
+                    {
+                      title: `Commodities imported (${importOrders.length})`,
+                      dataIndex: 'name',
+                      key: 'name',
+                      align: 'left',
+                      className: 'max-width-mobile',
+                      render: (v, r) =>
+                        <>
+                          <i className='icon icarus-terminal-cargo' />{v}<br />
+                          <div className='is-visible-mobile'>
+                            <small style={{ float: 'right' }}>{r.importOrders.length === 1 ? '1 importer ' : `${r.importOrders.length} importers`}</small>
+                          </div>
+                          <small>
+                            {r.category}
+                            {r?.consumer === true && ' (Consumer)'}
+                          </small>
+                          <div className='is-visible-mobile'>
+                            <table className='data-table--mini data-table--compact two-column-table'>
+                              <tbody style={{ textTransform: 'uppercase' }}>
+                                <tr>
+                                  <td><span className='data-table__label'>Total demand</span>{r.totalDemand > 0 ? `${r.totalDemand.toLocaleString()} T` : <small>{NO_DEMAND_TEXT}</small>}</td>
+                                  <td className='text-right'>
+                                    <span className='data-table__label'>Price</span>
+                                    {r.avgPrice.toLocaleString()} CR
+                                    <br />
+                                    {r.galacticAvgPrice > 0 && r.avgPrice > r.galacticAvgPrice && <small className='commodity__price text-positive'>+ {(r.avgPrice - r.galacticAvgPrice).toLocaleString()} CR</small>}
+                                    {r.avgPrice < r.galacticAvgPrice && <small className='commodity__price text-negative'>- {(r.galacticAvgPrice - r.avgPrice).toLocaleString()} CR</small>}
+                                  </td>
+                                </tr>
+                              </tbody>
+                            </table>
+                            <small style={{ textTransform: 'none' }}>{timeBetweenTimestamps(r.updatedAt)} ago</small>
+                          </div>
+                        </>
+                    },
+                    {
+                      title: 'Importers',
+                      dataIndex: 'importOrders',
+                      key: 'importOrders',
+                      align: 'center',
+                      width: 100,
+                      className: 'is-hidden-mobile',
+                      render: (v) => <span className='muted'>{v.length === 1 ? '1 ' : `${v.length}`}<i style={{ fontSize: '1.25rem', position: 'relative', top: '-.2rem', marginLeft: '.4rem' }} className='icarus-terminal-settlement muted' /></span>
+                    },
+                    {
+                      title: 'Updated',
+                      dataIndex: 'updatedAt',
+                      key: 'updatedAt',
+                      align: 'right',
+                      width: 150,
+                      className: 'is-hidden-mobile no-wrap',
+                      render: (v) => <span style={{ opacity: 0.5 }}>{timeBetweenTimestamps(v)}</span>
+                    },
+                    {
+                      title: 'Total demand',
+                      dataIndex: 'totalDemand',
+                      key: 'totalDemand',
+                      align: 'right',
+                      width: 200,
+                      className: 'is-hidden-mobile no-wrap',
+                      render: (v) => <>{v > 0 ? `${v.toLocaleString()} T` : <small>{NO_DEMAND_TEXT}</small>}</>
+                    },
+                    {
+                      title: 'Avg price',
+                      dataIndex: 'avgPrice',
+                      key: 'avgPrice',
+                      align: 'right',
+                      width: 150,
+                      className: 'is-hidden-mobile no-wrap',
+                      render: (v, r) =>
+                        <>
+                          {v.toLocaleString()} CR
+                          <br />
+                          {r.galacticAvgPrice > 0 && r.avgPrice > r.galacticAvgPrice && <small className='commodity__price text-positive'>+ {(r.avgPrice - r.galacticAvgPrice).toLocaleString()} CR</small>}
+                          {r.avgPrice < r.galacticAvgPrice && <small className='commodity__price text-negative'>- {(r.galacticAvgPrice - r.avgPrice).toLocaleString()} CR</small>}
+                        </>
+                    }
+                  ]}
+                  data={importOrders}
+                  rowKey={(r) => `system_imports_${r.systemAddress}_${r.symbol}`}
+                  expandable={{
+                    expandRowByClick: true,
+                    expandedRowRender: r =>
+                      <>
+                        <p style={{ marginTop: '.5rem' }}>
+                          Demand for <Link href={`/commodity/${r.symbol}/importers?location=${encodeURIComponent(r.systemName)}&maxDistance=100`}>{r.name}</Link> in {r.systemName}
+                        </p>
+                        <LocalCommodityImporters
+                          commodityName={r.name}
+                          commoditySymbol={r.symbol}
+                          commodityOrders={r.importOrders}
                         />
                         <Collapsible
                           trigger={<CollapsibleTrigger>Stock of <strong>{r.name}</strong> near <strong>{r.systemName}</strong></CollapsibleTrigger>}
