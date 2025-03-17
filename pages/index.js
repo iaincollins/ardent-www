5c05671e065bf034ce78bb08b98fb87ad5e4f773
Iain Collins <me@iaincollins.com>
2025-03-17 17:08:48 +0000

    Improved homepage UI

--- pages/index.js
+++ pages/index.js
@@ -107,17 +107,22 @@ export default (props) => {
             </h2>
           </div>
           <p style={{ marginTop: 0 }}>
-            Ardent Industry is the leading provider of trade data and market insights in the galaxy.
+            Ardent Industry is the leading provider of open trade data and market insights in the galaxy.
           </p>
           <p>
-            Our main offices are located on the Icarus Terminal station in <Link href='/system/Puppis%20Sector%20GB-X%20b1-5'>Puppis Sector GB-X b1-5</Link>.
+            Real time commodity data sourced from <Link target='_blank' href='https://eddn.edcd.io/'>EDDN</Link> relay.
           </p>
+          <h3 style={{ fontSize: '.9rem', lineHeight: '1rem' }}>Main Office</h3>
           <p>
-            Ardent Industry is a subsidiary of ICARUS Communications Corporation (ICC), creators of the <Link target='_blank' href='https://github.com/iaincollins/icarus'>ICARUS Terminal software</Link>.
+            <Link href='/system/Puppis%20Sector%20GB-X%20b1-5' className='text-uppercase' style={{ border: 0 }}>
+              <i className='icon icarus-terminal-outpost' />Icarus Terminal
+              <br />
+              <i className='icon icarus-terminal-star' />Puppis Sector GB-X b1-5</Link>
           </p>
-          <p>
-            Real time commodity data is provided by interlink with the <Link target='_blank' href='https://eddn.edcd.io/'>EDDN</Link> relay.
+          <p className='muted' style={{ fontSize: '.8rem' }}>
+            Ardent Industry is a subsidiary of ICARUS Communications Corporation (ICC), creators of <Link target='_blank' href='https://github.com/iaincollins/icarus'>ICARUS Terminal</Link>.
           </p>
+
         </div>
         <div className='news__feed'>
           <div className='heading--with-underline'>
@@ -133,7 +138,7 @@ export default (props) => {
               <div className='news__article-body'>
                 <img src={newsItem.image} width='100%' alt='News article headline' className='news__headline-image' />
                 <div className='news__article-text'>
-                  <p className='muted text-uppercase'><a href={newsItem.url}>Galnet News, {newsItem.date} </a></p>
+                  <p className='muted text-uppercase'><a target='_blank' href={newsItem.url}>Galnet News, {newsItem.date} </a></p>
                   <Markdown>{`${newsItem.text.replaceAll('\n', '\n\n')}`}</Markdown>
                 </div>
               </div>
@@ -144,175 +149,3 @@ export default (props) => {
     </Layout>
   )
 }
-
-
-/*
-
-export default function Page(props) {
-  const [navigationPath, setNavigationPath] = useContext(NavigationContext)
-
-  const router = useRouter()
-  const [commodities, setCommodities] = useState(props.commodities)
-  const [categories, setCategories] = useState(props.categories)
-
-  const onRowClick = (record, index, event) => {
-    router.push(`/commodity/${record.commodityName}`)
-  }
-
-  useEffect(animateTableEffect)
-
-  useEffect(() => {
-    (async () => {
-      const commodities_ = commodities ?? await getCommoditiesWithAvgPricing()
-
-      const filterByCategory = (router.query?.['commodity-category'])
-        ? commodities_.filter((c) => c.category.toLowerCase() === router.query?.['commodity-category'].toLowerCase())?.[0]?.category ?? false
-        : false
-
-      const categories_ = filterByCategory
-        ? [filterByCategory]
-        : [...new Set(commodities_.map((c) => c.category).sort())]
-
-      setCommodities(commodities_)
-      setCategories(categories_)
-
-      setNavigationPath(
-        (categories_.length === 1)
-          ? [{ name: 'Home', path: '/' }, { name: 'Commodities', path: '/commodities' }]
-          : [{ name: 'Home', path: '/' }, { name: 'Commodities', path: '/commodities' }]
-      )
-    })()
-  }, [router.asPath])
-
-  return (
-    <Layout
-      loading={commodities === undefined}
-      loadingText='Loading commodities'
-      title={categories?.length === 1 ? `${categories.filter(category => category.toLowerCase() !== 'nonmarketable').map(category => category)} in Elite Dangerous` : 'Commodities in Elite Dangerous'}
-      description={categories?.length === 1 ? `Where to buy and sell ${categories.filter(category => category.toLowerCase() !== 'nonmarketable').map(category => category)} in Elite Dangerous` : 'Where to buy and sell commodities in Elite Dangerous'}
-    >
-      <Head>
-        <link rel='canonical' href='https://ardent-industry.com/commodities' />
-      </Head>
-      {commodities && categories &&
-        <div className='fx__fade-in'>
-          {categories?.length > 1 &&
-            <>
-              <div className='heading--with-underline'>
-                <h2 className='heading--with-icon'>
-                  <i className='icon icarus-terminal-cargo' />
-                  Commodities
-                </h2>
-              </div>
-              <p className='text-center muted' style={{marginTop: 0}}>
-                Commodity prices and supply / demand updated in real time via <Link target='_blank' href='https://eddn.edcd.io/'>EDDN</Link>.
-              </p>
-            </>}
-          {categories?.length === 1 && commodityCategories[categories[0]]?.description && <>
-            <div className='heading--with-underline'>
-              <h2 className='heading--with-icon'>
-                <i className='icon icarus-terminal-cargo' />
-                {categories}
-              </h2>
-            </div>
-            <p className='clear text-centerx' style={{ marginTop: 0, marginBottom: '.5rem' }}>
-              {commodityCategories[categories[0]].description}
-              {categories?.length === 1 && commodityCategories[categories[0]]?.whereToFind && <>
-                {' '}{commodityCategories[categories[0]]?.whereToFind}
-              </>}
-            </p>
-          </>}
-          {categories?.length > 1 &&
-            <div className='menu-button-grid'>
-              {categories.filter(category => category.toLowerCase() !== 'nonmarketable').map(category =>
-                <Link className='button' href={`/commodities/${category.toLowerCase()}`}>{category}</Link>
-              )}
-            </div>
-          }
-          {categories?.length === 1 && categories.filter(category => category.toLowerCase() !== 'nonmarketable').map(category =>
-            <div key={`category_${category}`} style={{ marginTop: '1rem' }}>
-              {categories?.length > 1 && <h2 className='heading--table' onClick={() => router.push(`/commodities/${category.toLowerCase()}`)}>{category}</h2>}
-              <Table
-                className='data-table data-table--striped data-table--interactive data-table--animated'
-                columns={[
-                  {
-                    title: 'Commodity',
-                    dataIndex: 'name',
-                    key: 'commodityName',
-                    align: 'left',
-                    className: 'max-width-mobile',
-                    render: (v, r) =>
-                      <>
-                        <i className='icon icarus-terminal-cargo' />
-                        {v}<br />
-                        <small>
-                          {r.category}
-                          {r.market_id && <>, RARE</>}
-                        </small>
-                        <div className='is-visible-mobile'>
-                          <table className='data-table--mini data-table--compact two-column-table'>
-                            <tbody style={{ textTransform: 'uppercase' }}>
-                              <tr>
-                                <td><span className='data-table__label'>Avg Export CR/T</span>{r.avgBuyPrice > 0 ? <>{r.avgBuyPrice.toLocaleString()} CR</> : '-'}</td>
-
-                                <td><span className='data-table__label'>Avg Profit CR/T</span>{r.avgProfit > 0 ? <>{r.avgProfit.toLocaleString()} CR</> : '-'}</td>
-                              </tr>
-                              <tr>
-                                <td><span className='data-table__label'>Avg Import CR/T</span>
-                                  {r.avgSellPrice > 0 ? <>{r.avgSellPrice.toLocaleString()} CR</> : '-'}
-                                </td>
-
-                                <td><span className='data-table__label'>Max Profit CR/T</span>{r.maxProfit > 0 ? <>{r.maxProfit.toLocaleString()} CR</> : '-'}</td>
-                              </tr>
-                            </tbody>
-                          </table>
-                        </div>
-                      </>
-                  },
-                  {
-                    title: 'Avg export CR/T',
-                    dataIndex: 'avgBuyPrice',
-                    key: 'avgBuyPrice',
-                    align: 'right',
-                    className: 'is-hidden-mobile no-wrap',
-                    width: 150,
-                    render: (v) => v > 0 ? <>{v.toLocaleString()} CR</> : <small>-</small>
-                  },
-                  {
-                    title: 'Avg import CR/T',
-                    dataIndex: 'avgSellPrice',
-                    key: 'avgSellPrice',
-                    align: 'right',
-                    className: 'is-hidden-mobile no-wrap',
-                    width: 150,
-                    render: (v, r) => (v > 0) ? <>{v.toLocaleString()} CR</> : <small>-</small>
-                  },
-                  {
-                    title: 'Avg profit CR/T',
-                    dataIndex: 'avgProfit',
-                    key: 'avgProfit',
-                    align: 'right',
-                    width: 150,
-                    className: 'is-hidden-mobile no-wrap',
-                    render: (v, r) =>
-                      <div style={{ textTransform: 'uppercase' }}>
-                        {v > 0 ? <>{v.toLocaleString()} CR</> : <small>-</small>}
-                        <br />
-                        {v > 0 ? <small>Max {r.maxProfit.toLocaleString()} CR</small> : ''}
-                      </div>
-                  }
-                ]}
-                data={commodities.filter(c => c.category.toLowerCase() === category.toLowerCase())}
-                rowKey='name'
-                onRow={(record, index) => ({
-                  onClick: onRowClick.bind(null, record, index)
-                })}
-              />
-            </div>
-          )}
-        </div>}
-    </Layout>
-  )
-}
-
-*/
